b40d1f9bb664982c405be8006b7fd9446ea739f0
timcs <timcs1274@gmail.com>
2023-02-27 13:57:08 -0500

    add basic html to home.js

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <header className='heading'>
+        </header>
+        <main>
+            <div className='aboutCont'>
+                <h2>Mark W. Sasse</h2>
+                <img id='headshot' src='./assets/images/mark-headshot.webp' alt='Author'></img>
+            </div>
+            <div className='newReleases'>New Releases</div>
+            <div className='newsletter'>
+                <h2>Get all the latest MWS news</h2>
+                <div>form</div>
+            </div>
+        </main>
+    </div>
   )
 }
 
