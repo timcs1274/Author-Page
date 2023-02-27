e9312239316bd78c28db25439d61911f583ac524
timcs <timcs1274@gmail.com>
2023-02-27 13:16:14 -0500

    add basic routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Blog from './pages/Blog';
+import Books from './pages/Books';
+import Podcast from './pages/Podcast';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/blog' element={<Blog />} />
+          <Route path='/books' element={<Books />} />
+          <Route path='/podcasts' element={<Podcast />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
