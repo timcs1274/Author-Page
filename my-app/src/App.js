d6efa19d3f78dfacfc21749eb39e58f6829c864e
timcs <timcs1274@gmail.com>
2023-02-27 13:32:05 -0500

    add router dom dependency and material ui

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
           <Route path='/blog' element={<Blog />} />
           <Route path='/books' element={<Books />} />
           <Route path='/podcasts' element={<Podcast />} />
