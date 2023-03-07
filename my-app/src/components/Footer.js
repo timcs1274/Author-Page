184e62ba4009699cb8e045f5f65c654709becf40
timcs <timcs1274@gmail.com>
2023-03-07 16:09:01 -0500

    style footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -3,19 +3,21 @@ import '../styles/Footer.css'
 
 function Footer() {
   return (
-    <div>
+    <div className='footerContainer'>
         <div className='footerTitle'>Mark W Sasse</div>
         <hr></hr>
-            <div className='footerMedia'></div>
-            <div className='footerWorks'>Works</div>
-                <div className='footerBooks'>Books</div>
-                <div className='footerBlogs'>Blogs</div>
-                <div className='footerPodcasts'>Podcasts</div>
-            <div className='footerAbout'>About Mark</div>
-                <div className='footerAbout'>Biography</div>
-                <div className='footerContact'>Contact</div>
-                <div className='footerNews'>Newsletter</div>
-            <div></div>
+            <div className='footerMedia'>
+            <div className='footerWorks'>Works
+                <p className='footerBooks'>Books</p>
+                <p className='footerBlogs'>Blogs</p>
+                <p className='footerPodcasts'>Podcasts</p>
+            </div>
+            <div className='footerAbout'>About
+                <p className='footerAbout'>Biography</p>
+                <p className='footerContact'>Contact</p>
+                <p className='footerNews'>Newsletter</p>
+            </div>
+            </div>
     </div>
   )
 }
